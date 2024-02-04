820a39676031d2471f58db5791eb32294779fd8c
Aleksandr <pulatov.alexander@gmail.com>
2024-02-04 12:48:52 +0300

    Initial Commit

---
+++ BirthdayTracker_Swift-CodeUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayTracker_Swift-CodeUI
+//
+//  Created by Alexander on 4.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
